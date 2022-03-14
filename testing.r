71d7c7318bfa3c4bbb8973a80fa321e87476fe8a
Yasir Saeed <yasirsaeedcfo@gmail.com>
2022-03-14 09:20:37 -0700

    Few Lines added

--- testing.r
+++ testing.r
@@ -1,3 +1,15 @@
 # This is from Github
 print("Hello from Github")
 
+name = "Sana"
+Version = "RStudio - In writing"
+
+print(name)
+print("These lines have been written in RStudio")
+name = "Sana"
+print(name)
+first = ("Yasir")
+print(first)
+print(Version)
+last="saima"
+print(last)
